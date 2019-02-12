4e90aeba4023f99f5daff8ac8e003d79891d08a3
Chris Sergienko <chris@praekelt.com>
2019-02-12 22:21:45 +0200

    chore: Clean up index page.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,12 @@
-import React from "react"
-import { Link } from "gatsby"
-
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import React from 'react';
+import SEO from '../components/seo';
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+    <center style={{ marginTop: '100px' }}>
+        <SEO title="I Prefer the Front-End" />
+        <h1>Ain't nothing to see here. Yet.</h1>
+        <img src="https://media.giphy.com/media/voZqawzMMG4Lu/giphy.gif" />
+    </center>
+);
 
-export default IndexPage
+export default IndexPage;
